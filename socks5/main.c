e4657c01edaa127c4dcbcd7ecc928b58595f5f02
name327 <name327@qq.com>
2020-06-14 16:58:31 +0800

    忽略SIGPIPE信号

--- socks5/main.c
+++ socks5/main.c
@@ -7,14 +7,16 @@
 //
 
 #include <stdio.h>
+#include <signal.h>
 #include "socks5.h"
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     struct socks5_svr svr;
     svr.port=4567;
+    //忽略SIGPIPE信号，防止socket写入出错，产生SIGPIPE信号，导致进程退出
+    signal(SIGPIPE, SIG_IGN);
     socks5_start(&svr);
-    
-    
     return 0;
 }
